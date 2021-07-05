0d80b82b23620f354133b2d495a2bf29b7a96b75
fairbird <rrrr53@hotmail.com>
2021-07-05 15:40:59 +0300

    Fix some crash and add some missing codes

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -3842,7 +3842,7 @@ class InfoBarHdmi2:
 		self.hdmi_enabled_full = False
 		self.hdmi_enabled_pip = False
 
-		if BoxInfo.getItem("HasHDMIin"):
+		if SystemInfo["HasHDMIin"]:
 			if not self.hdmi_enabled_full:
 				self.addExtension((self.getHDMIInFullScreen, self.HDMIInFull, lambda: True), "blue")
 			if not self.hdmi_enabled_pip:
