b06b217415332e279cd40bbe6ace5e08bd3c57b2
Hains <hainsvdbosch@ziggo.nl>
2021-07-06 19:21:49 +0200

    About.py: Fix driver date dm8000

--- lib/python/Components/About.py
+++ lib/python/Components/About.py
@@ -159,7 +159,10 @@ def getDriverInstalledDate():
 	try:
 		from glob import glob
 		try:
-			driver = [x.split("-")[-2:-1][0][-8:] for x in open(glob("/var/lib/opkg/info/*-dvb-modules-*.control")[0], "r") if x.startswith("Version:")][0]
+			if HardwareInfo().get_device_name() =="dm8000":
+				driver = [x.split("-")[-2:-1][0][-9:] for x in open(glob("/var/lib/opkg/info/*-dvb-modules-*.control")[0], "r") if x.startswith("Version:")][0]
+			else:
+				driver = [x.split("-")[-2:-1][0][-8:] for x in open(glob("/var/lib/opkg/info/*-dvb-modules-*.control")[0], "r") if x.startswith("Version:")][0]
 			return "%s-%s-%s" % (driver[:4], driver[4:6], driver[6:])
 		except:
 			try:
