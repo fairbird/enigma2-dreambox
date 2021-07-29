b5622d290057823c47760a4a0f6b61a03ec5a492
RAED <rrrr53@hotmail.com>
2021-07-29 14:49:43 +0300

    [EpgList] Fix error (NameError: global name 'split' is not defined)
    2021-07-29 12:47:11+0100 [-]   File "/usr/lib/enigma2/python/Components/EpgList.py", line 248, in buildSingleEntry
    2021-07-29 12:47:11+0100 [-]     (eListboxPythonMultiContent.TYPE_TEXT, r2.x, r2.y, split, r2.h, 0, RT_HALIGN_RIGHT | RT_VALIGN_CENTER, strftime(config.usage.time.short.value + " -", t)),
    2021-07-29 12:47:11+0100 [-] NameError: global name 'split' is not defined

--- lib/python/Components/EpgList.py
+++ lib/python/Components/EpgList.py
@@ -240,6 +240,7 @@ class EPGList(GUIComponent):
                 r1 = self.weekday_rect
                 r2 = self.datetime_rect
                 r3 = self.descr_rect
+                split = int(r2.w * 0.55)
                 t = localtime(beginTime)
                 et = localtime(beginTime + duration)
                 res = [
