24f0e2bb731faf91509ac620c53a1e5ce63ff849
fairbird <rrrr53@hotmail.com>
2021-09-21 14:17:29 +0300

    Silence format warnings

--- lib/service/servicedvbrecord.cpp
+++ lib/service/servicedvbrecord.cpp
@@ -590,7 +590,7 @@ void eDVBServiceRecord::fixupCuts(std::list<pts_t> &offsets)
 			eDebug("[eDVBServiceRecord] fixing up PTS failed, not saving");
 			continue;
 		}
-		eDebug("[eDVBServiceRecord] fixed up %llx to %llx (offset %llx)", i->second, p, offset);
+		//eDebug("[eDVBServiceRecord] fixed up %llx to %llx (offset %llx)", i->second, p, offset);
 		offsets.push_back(p);
 	}
 }
