5fdf8a602ae7cd153da87586cc59f98d21c48093
Betacentauri <betacentauri@arcor.de>
2021-11-15 14:12:03 +0100

    Remove ci plus version check
    Not all boxes support the needed ioctl. E.g. dm8000 resets the
    module and this causes an endless loop.

--- lib/dvb_ci/dvbci.cpp
+++ lib/dvb_ci/dvbci.cpp
@@ -1273,7 +1273,6 @@ void eDVBCISlot::data(int what)
 	if(state != stateInserted) {
 		eDebug("[CI] ci inserted in slot %d", getSlotID());
 		state = stateInserted;
-		determineCIVersion();
 		/* emit */ eDVBCI_UI::getInstance()->m_messagepump.send(eDVBCIInterfaces::Message(eDVBCIInterfaces::Message::slotStateChanged, getSlotID(), 1));
 		notifier->setRequested(eSocketNotifier::Read|eSocketNotifier::Priority);
 		/* enable PRI to detect removal or errors */
