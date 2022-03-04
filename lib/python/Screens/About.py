07c77ae7f6e862662278ddee6fa9b90caf27f81c
fairbird <rrrr53@hotmail.com>
2022-03-04 11:29:19 +0300

    [About] Restore last update date time

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -35,7 +35,9 @@ class About(Screen):
 		AboutText += _("CPU: ") + cpu + "\n"
 		AboutText += _("Image: ") + about.getImageTypeString() + "\n"
 		AboutText += _("Build date: ") + about.getBuildDateString() + "\n"
-		AboutText += _("Last update: ") + about.getUpdateDateString() + "\n"
+		ImageVersion = _("Last update: ") + about.getImageVersionString()
+		self["ImageVersion"] = StaticText(ImageVersion)
+		AboutText += ImageVersion + "\n"
 
 		# [WanWizard] Removed until we find a reliable way to determine the installation date
 		# AboutText += _("Installed: ") + about.getFlashDateString() + "\n"
