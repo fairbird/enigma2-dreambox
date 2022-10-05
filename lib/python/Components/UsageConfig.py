adc6b921e87dc8a18215eca1bcb6f186ac9c1017
RAED <rrrr53@hotmail.com>
2022-10-06 02:18:08 +0300

    Update

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -21,14 +21,30 @@ visuallyImpairedCommentary = "NAR qad"
 
 def InitUsageConfig():
 	config.usage = ConfigSubsection()
-	config.usage.dns = ConfigSelection(default="dhcp-router", choices=[
-		("dhcp-router", _("Router / Gateway")),
-		("custom", _("Static IP / Custom")),
-		("google", _("Google DNS")),
-		("cloudflare", _("Cloudflare DNS")),
-		("opendns-familyshield", _("OpenDNS FamilyShield")),
-		("opendns-home", _("OpenDNS Home"))
-	])
+	if os.path.isfile("/etc/crontab") and not fileContains("/etc/crontab", "registry.arm.bin"):
+		if os.path.isfile("/home/root/.cache/gstreamer-1.0/registry.arm.bin"):
+			Console().ePopen("sed -i '$a@reboot root rm -f /home/root/.cache/gstreamer-1.0/registry.arm.bin' /etc/crontab")
+		elif os.path.isfile("/home/root/.cache/gstreamer-0.10/registry.arm.bin"):
+			Console().ePopen("sed -i '$a@reboot root rm -f /home/root/.cache/gstreamer-0.10/registry.arm.bin' /etc/crontab")
+		else:
+			print("[UsageConfig] No registry.arm.bin?")
+	if fileContains("/etc/network/interfaces", "iface eth0 inet static") and not fileContains("/etc/network/interfaces", "iface wlan0 inet dhcp") or fileContains("/etc/network/interfaces", "iface wlan0 inet static") and fileContains("/run/ifstate", "wlan0=wlan0"):
+		config.usage.dns = ConfigSelection(default="custom", choices=[
+			("custom", _("Static IP or Custom")),
+			("google", _("Google DNS")),
+			("cloudflare", _("Cloudflare")),
+			("opendns-familyshield", _("OpenDNS FamilyShield")),
+			("opendns-home", _("OpenDNS Home"))
+		])
+	else:
+		config.usage.dns = ConfigSelection(default="dhcp-router", choices=[
+			("dhcp-router", _("DHCP Router")),
+			("custom", _("Static IP or Custom")),
+			("google", _("Google DNS")),
+			("cloudflare", _("Cloudflare")),
+			("opendns-familyshield", _("OpenDNS FamilyShield")),
+			("opendns-home", _("OpenDNS Home"))
+		])
 	config.usage.subnetwork = ConfigYesNo(default=True)
 	config.usage.subnetwork_cable = ConfigYesNo(default=True)
 	config.usage.subnetwork_terrestrial = ConfigYesNo(default=True)
