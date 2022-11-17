0e79ee9bd7c4cedc03e467045bb8f34c3f1d48c6
fairbird <rrrr53@hotmail.com>
2022-11-18 00:34:02 +0300

    [Times] Update HD skin for TimeWizard

--- lib/python/Screens/Time.py
+++ lib/python/Screens/Time.py
@@ -70,18 +70,18 @@ class Time(Setup):
 class TimeWizard(ConfigListScreen, Screen, Rc):
 	if isHD():
 		skin = """
-		<screen name="TimeWizard" position="center,60" size="980,635" resolution="1280,720">
-			<widget name="text" position="10,10" size="e-20,25" font="Regular;20" transparent="1" valign="center" />
-			<widget name="config" position="10,40" size="e-20,250" enableWrapAround="1" entryFont="Regular;25" valueFont="Regular;25" itemHeight="35" scrollbarMode="showOnDemand" />
-			<widget source="key_red" render="Label" objectTypes="key_red,StaticText" position="180,e-50" size="180,40" backgroundColor="key_red" conditional="key_red" font="Regular;20" foregroundColor="key_text" halign="center" valign="center">
+		<screen name="TimeWizard" position="center,60" size="1280,720" resolution="1280,720">
+			<widget name="text" position="30,10" size="1000,47" font="Regular;20" transparent="1" valign="center" />
+			<widget name="config" position="212,95" size="944,430" enableWrapAround="1" entryFont="Regular;25" valueFont="Regular;25" itemHeight="35" scrollbarMode="showOnDemand" />
+			<widget source="key_red" render="Label" objectTypes="key_red,StaticText" position="234,670" size="180,40" backgroundColor="key_red" conditional="key_red" font="Regular;20" foregroundColor="key_text" halign="center" valign="center">
 				<convert type="ConditionalShowHide" />
 			</widget>
-			<widget source="key_yellow" render="Label" objectTypes="key_yellow,StaticText" position="390,e-50" size="180,40" backgroundColor="key_yellow" conditional="key_yellow" font="Regular;20" foregroundColor="key_text" halign="center" valign="center">
+			<widget source="key_yellow" render="Label" objectTypes="key_yellow,StaticText" position="519,670" size="180,40" backgroundColor="key_yellow" conditional="key_yellow" font="Regular;20" foregroundColor="key_text" halign="center" valign="center">
 				<convert type="ConditionalShowHide" />
 			</widget>
 			<widget name="HelpWindow" position="0,0" size="0,0" alphatest="blend" conditional="HelpWindow" transparent="1" zPosition="+1" />
-			<widget name="rc" conditional="rc" alphatest="blend" position="10,290" size="100,360" />
-			<widget name="wizard" conditional="wizard" pixmap="picon_default.png" position="740,400" size="220,132" alphatest="blend" />
+			<widget name="rc" conditional="rc" alphatest="blend" position="24,95" size="163,524" />
+			<widget name="wizard" conditional="wizard" pixmap="picon_default.png" position="1044,582" size="220,132" alphatest="blend"/>
 			<widget name="indicatorU0" pixmap="skin_default/yellow_circle23x23.png" position="0,0" size="23,23" alphatest="blend" offset="11,11" zPosition="11" />
 			<widget name="indicatorU1" pixmap="skin_default/yellow_circle23x23.png" position="0,0" size="23,23" alphatest="blend" offset="11,11" zPosition="11" />
 			<widget name="indicatorU2" pixmap="skin_default/yellow_circle23x23.png" position="0,0" size="23,23" alphatest="blend" offset="11,11" zPosition="11" />
