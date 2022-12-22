89c1feb47646c1841cc0af1ab4cc044add855a8f
fairbird <rrrr53@gotmail.com>
2022-12-23 02:30:17 +0300

    [Input] Py3 end() require textU too. Depend on https://github.com/openatv/enigma2/commit/f68b5e544340dd07f49cd2764020b6f7e37f2e72

--- lib/python/Components/Input.py
+++ lib/python/Components/Input.py
@@ -103,10 +103,7 @@ class Input(VariableText, GUIComponent, NumericalTextInput):
 		if self.type == self.TEXT:
 			self.timeout()
 		if self.allmarked:
-			if self.maxSize:
-				self.currPos = len(self.Text) - 1
-			else:
-				self.currPos = len(self.Text)
+			self.currPos = len(self.textU) - 1 if self.maxSize else len(self.textU)
 			self.allmarked = False
 		elif self.currPos > 0:
 			self.currPos -= 1
