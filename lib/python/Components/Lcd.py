76574b2359581595f0f4fa0f0554c89e92bfeb9a
fairbird <rrrr53@hotmail.com>
2023-02-03 23:02:53 +0300

    Lcd.py: update

--- lib/python/Components/Lcd.py
+++ lib/python/Components/Lcd.py
@@ -2,7 +2,6 @@ from os import sys
 from os.path import exists
 from sys import maxsize
 from twisted.internet import threads
-from six import PY2
 
 from enigma import eActionMap, eDBoxLCD, eTimer
 
@@ -69,17 +68,15 @@ class IconCheckPoller:
 			fileWriteLine("/proc/stb/lcd/symbol_network", linkState)
 		elif exists("/proc/stb/lcd/symbol_network") and config.lcd.mode.value == "0":
 			fileWriteLine("/proc/stb/lcd/symbol_network", "0")
-		if PY2:
-			from usb import busses
-			USBState = 0
-			for bus in busses():
-				devices = bus.devices
-				for dev in devices:
-					if dev.deviceClass != 9 and dev.deviceClass != 2 and dev.idVendor != 3034 and dev.idVendor > 0:
-						USBState = 1
-			if exists("/proc/stb/lcd/symbol_usb"):
-				fileWriteLine("/proc/stb/lcd/symbol_usb", USBState)
-			self.timer.startLongTimer(30)
+		USBState = 0
+		for bus in busses():
+			devices = bus.devices
+			for dev in devices:
+				if dev.deviceClass != 9 and dev.deviceClass != 2 and dev.idVendor != 3034 and dev.idVendor > 0:
+					USBState = 1
+		if exists("/proc/stb/lcd/symbol_usb"):
+			fileWriteLine("/proc/stb/lcd/symbol_usb", USBState)
+		self.timer.startLongTimer(30)
 
 
 class LCD:
@@ -544,7 +541,7 @@ def InitLcd():
 
 		config.lcd.standby = ConfigSlider(default=standby_default, limits=(0, 10))
 		config.lcd.dimbright = ConfigSlider(default=standby_default, limits=(0, 10))
-		config.lcd.bright = ConfigSlider(SystemInfo["DefaultDisplayBrightness"], limits=(0, 10))
+		config.lcd.bright = ConfigSlider(default=SystemInfo["DefaultDisplayBrightness"], limits=(0, 10))
 		config.lcd.dimbright.addNotifier(setLCDdimbright)
 		config.lcd.dimbright.apply = lambda: setLCDdimbright(config.lcd.dimbright)
 		config.lcd.dimdelay = ConfigSelection(choices=[
