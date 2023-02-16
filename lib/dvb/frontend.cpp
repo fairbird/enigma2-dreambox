2aadbf5019f8f3995d5cbacadc982d67b8e8c1ca
zhtq <36952536+zhtq@users.noreply.github.com>
2023-02-16 11:51:32 +0800

    add av62x1 and gService demod

--- lib/dvb/frontend.cpp
+++ lib/dvb/frontend.cpp
@@ -1374,7 +1374,15 @@ void eDVBFrontend::calculateSignalQuality(int snr, int &signalquality, int &sign
 	{
 		ret = snr;
 	}
-	
+	else if (!strcmp(m_description, "AVL62X1"))
+	{
+		ret = snr;
+	}
+	else if (!strcmp(m_description, "gService DVB-S2")) // SX88V2
+	{
+		ret = snr;
+	}
+
 	signalqualitydb = ret;
 	if (ret == 0x12345678) // no snr db calculation avail.. return untouched snr value..
 	{
