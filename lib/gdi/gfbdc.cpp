2e49bd07733c50e9263a8ea59995de41fce55c6e
fairbird <rrrr53@hotmail.com>
2023-05-24 22:56:58 +0300

    update

--- lib/gdi/gfbdc.cpp
+++ lib/gdi/gfbdc.cpp
@@ -11,7 +11,11 @@
 #include <vuplus_gles.h>
 #endif
 
-#ifdef CONFIG_ION
+#ifdef HAVE_OSDANIMATION
+#include <lib/base/cfile.h>
+#endif
+
+#if defined(CONFIG_ION) || defined(CONFIG_HISILICON_FB)
 #include <lib/gdi/grc.h>
 
 extern void bcm_accel_blit(
@@ -20,6 +24,10 @@ extern void bcm_accel_blit(
 		int src_x, int src_y, int width, int height,
 		int dst_x, int dst_y, int dwidth, int dheight,
 		int pal_addr, int flags);
+#endif
+#ifdef HAVE_HISILICON_ACCEL
+extern void  dinobot_accel_register(void *p1,void *p2);
+extern void  dinibot_accel_notify(void);
 #endif
 
 gFBDC::gFBDC()
@@ -159,7 +167,15 @@ void gFBDC::exec(const gOpcode *o)
 		break;
 	}
 	case gOpcode::flush:
+#ifdef USE_LIBVUGLES2
+		if (gles_is_animation())
+			gles_do_animation();
+		else
+			fb->blit();
+		gles_flush();
+#else
 		fb->blit();
+#endif
 #ifdef CONFIG_ION
 		if (surface_back.data_phys)
 		{
@@ -184,7 +200,66 @@ void gFBDC::exec(const gOpcode *o)
 				0, 0);
 		}
 #endif
+#if defined(CONFIG_HISILICON_FB)
+		if(islocked()==0)
+		{
+			bcm_accel_blit(
+				surface.data_phys, surface.x, surface.y, surface.stride, 0,
+				surface_back.data_phys, surface_back.x, surface_back.y, surface_back.stride,
+				0, 0, surface.x, surface.y,
+				0, 0, surface.x, surface.y,
+				0, 0);
+		}
+#endif
+#ifdef HAVE_HISILICON_ACCEL
+		dinibot_accel_notify();
+#endif
 		break;
+	case gOpcode::sendShow:
+	{
+#ifdef HAVE_OSDANIMATION
+		CFile::writeIntHex("/proc/stb/fb/animation_mode", 0x01);
+#endif
+#ifdef USE_LIBVUGLES2
+		gles_set_buffer((unsigned int *)surface.data);
+		gles_set_animation(1, o->parm.setShowHideInfo->point.x(), o->parm.setShowHideInfo->point.y(), o->parm.setShowHideInfo->size.width(), o->parm.setShowHideInfo->size.height());
+#endif
+		delete o->parm.setShowHideInfo;
+		break;
+	}
+	case gOpcode::sendHide:
+	{
+#ifdef HAVE_OSDANIMATION
+		CFile::writeIntHex("/proc/stb/fb/animation_mode", 0x10);
+#endif
+#ifdef USE_LIBVUGLES2
+		gles_set_buffer((unsigned int *)surface.data);
+		gles_set_animation(0, o->parm.setShowHideInfo->point.x(), o->parm.setShowHideInfo->point.y(), o->parm.setShowHideInfo->size.width(), o->parm.setShowHideInfo->size.height());
+#endif
+		delete o->parm.setShowHideInfo;
+		break;
+	}
+#ifdef USE_LIBVUGLES2
+	case gOpcode::sendShowItem:
+	{
+		gles_set_buffer((unsigned int *)surface.data);
+		gles_set_animation_listbox(o->parm.setShowItemInfo->dir, o->parm.setShowItemInfo->point.x(), o->parm.setShowItemInfo->point.y(), o->parm.setShowItemInfo->size.width(), o->parm.setShowItemInfo->size.height());
+		delete o->parm.setShowItemInfo;
+		break;
+	}
+	case gOpcode::setFlush:	
+	{
+		gles_set_flush(o->parm.setFlush->enable);
+		delete o->parm.setFlush;
+		break;
+	}
+	case gOpcode::setView:
+	{
+		gles_viewport(o->parm.setViewInfo->size.width(), o->parm.setViewInfo->size.height(), fb->Stride());
+		delete o->parm.setViewInfo;
+		break;
+	}
+#endif
 	default:
 		gDC::exec(o);
 		break;
@@ -217,7 +292,11 @@ void gFBDC::setGamma(int g)
 
 void gFBDC::setResolution(int xres, int yres, int bpp)
 {
-	if (m_pixmap && (surface.x == xres) && (surface.y == yres) && (surface.bpp == bpp))
+	if (m_pixmap && (surface.x == xres) && (surface.y == yres) && (surface.bpp == bpp)
+	#if defined(CONFIG_HISILICON_FB)
+		&& islocked()==0
+	#endif
+		)
 		return;
 #ifndef CONFIG_ION
 	if (gAccel::getInstance())
@@ -262,6 +341,9 @@ void gFBDC::setResolution(int xres, int yres, int bpp)
 	if (gAccel::getInstance())
 		gAccel::getInstance()->setAccelMemorySpace(fb->lfb + fb_size, surface.data_phys + fb_size, fb->Available() - fb_size);
 #endif
+#ifdef HAVE_HISILICON_ACCEL
+	dinobot_accel_register(&surface,&surface_back);
+#endif
 	if (!surface.clut.data)
 	{
 		surface.clut.colors = 256;
@@ -270,6 +352,16 @@ void gFBDC::setResolution(int xres, int yres, int bpp)
 	}
 
 	surface_back.clut = surface.clut;
+
+#if defined(CONFIG_HISILICON_FB)
+	if(islocked()==0)
+	{
+		gUnmanagedSurface s(surface);
+		surface = surface_back;
+		surface_back = s;
+	}
+#endif
+
 	m_pixmap = new gPixmap(&surface);
 
 #ifdef CONFIG_ION
@@ -293,3 +385,61 @@ void gFBDC::reloadSettings()
 }
 
 eAutoInitPtr<gFBDC> init_gFBDC(eAutoInitNumbers::graphic-1, "GFBDC");
+
+#ifdef HAVE_OSDANIMATION
+void setAnimation_current(int a) {
+	switch (a) {
+		case 1:
+			CFile::writeStr("/proc/stb/fb/animation_current", "simplefade");
+			break;
+		case 2:
+			CFile::writeStr("/proc/stb/fb/animation_current", "simplezoom");
+			break;
+		case 3:
+			CFile::writeStr("/proc/stb/fb/animation_current", "growdrop");
+			break;
+		case 4:
+			CFile::writeStr("/proc/stb/fb/animation_current", "growfromleft");
+			break;
+		case 5:
+			CFile::writeStr("/proc/stb/fb/animation_current", "extrudefromleft");
+			break;
+		case 6:
+			CFile::writeStr("/proc/stb/fb/animation_current", "popup");
+			break;
+		case 7:
+			CFile::writeStr("/proc/stb/fb/animation_current", "slidedrop");
+			break;
+		case 8:
+			CFile::writeStr("/proc/stb/fb/animation_current", "slidefromleft");
+			break;
+		case 9:
+			CFile::writeStr("/proc/stb/fb/animation_current", "slidelefttoright");
+			break;
+		case 10:
+			CFile::writeStr("/proc/stb/fb/animation_current", "sliderighttoleft");
+			break;
+		case 11:
+			CFile::writeStr("/proc/stb/fb/animation_current", "slidetoptobottom");
+			break;
+		case 12:
+			CFile::writeStr("/proc/stb/fb/animation_current", "zoomfromleft");
+			break;
+		case 13:
+			CFile::writeStr("/proc/stb/fb/animation_current", "zoomfromright");
+			break;
+		case 14:
+			CFile::writeStr("/proc/stb/fb/animation_current", "stripes");
+			break;
+		default:
+			CFile::writeStr("/proc/stb/fb/animation_current", "disable");
+			break;
+	}
+}
+
+void setAnimation_speed(int speed) {
+	CFile::writeInt("/proc/stb/fb/animation_speed", speed);
+}
+
+void setAnimation_current_listbox(int a) {}
+#endif
