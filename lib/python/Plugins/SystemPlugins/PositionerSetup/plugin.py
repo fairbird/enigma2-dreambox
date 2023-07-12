e75c446969ab01b5caaff9712275a7c25b18d8c7
fairbird <rrrr53@hotmail.com>
2023-07-12 22:02:02 +0300

    [PositionerSetup] Update

--- lib/python/Plugins/SystemPlugins/PositionerSetup/plugin.py
+++ lib/python/Plugins/SystemPlugins/PositionerSetup/plugin.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from __future__ import print_function
 from __future__ import absolute_import
 from enigma import eTimer, eDVBResourceManager, eDVBDiseqcCommand, eDVBFrontendParametersSatellite, iDVBFrontend
