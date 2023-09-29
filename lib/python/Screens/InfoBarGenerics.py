44879991c636b169810592f4535cd40fa0e462f7
Taapat <taapat@gmail.com>
2023-09-29 17:47:16 +0300

    Update also summarylist when refreshes the extensions list

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -2309,6 +2309,7 @@ class ExtensionsList(ChoiceBox):
 			if x[0][0] != text:  # Update text if changed
 				x[0] = (text, *x[0][1:])
 				x[1] = x[1][:7] + (text,)
+				self.summarylist[idx] = (self.summarylist[idx][0], text)
 				updated = True
 			elif not x[0][1][2]():  # Remove job if not active
 				updated = True
@@ -2400,7 +2401,6 @@ class InfoBarExtensions:
 					extensionsList.remove(extension)
 		clist.extend([(x[0](), x) for x in extensionsList])
 		clist and self.session.openWithCallback(self.extensionCallback, ExtensionsList, clist=clist, keys=keys, refresh_list="refresh" in self.extensionKeys)
-
 
 	def extensionCallback(self, answer):
 		if answer is not None:
