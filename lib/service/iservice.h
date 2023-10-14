2790b67dffc7b30a71f02be0892d7f218a67f965
RAED <rrrr53@hotmail.com>
2023-10-14 14:49:08 +0300

    Update iservice.h

--- lib/service/iservice.h
+++ lib/service/iservice.h
@@ -53,9 +53,11 @@ public:
 #ifndef SWIG
 	int data[8];
 	std::string path;
+	std::string suburi;
 #endif
 	std::string getPath() const { return path; }
 	void setPath( const std::string &n ) { path=n; }
+	void setSubUri( const std::string &n ) { suburi=n; }
 
 	unsigned int getUnsignedData(unsigned int num) const
 	{
@@ -724,6 +726,7 @@ public:
 		int page_number;
 		int magazine_number;
 		std::string language_code;
+		std::string title;
 	};
 
 	virtual RESULT enableSubtitles(iSubtitleUser *user, SubtitleTrack &track) = 0;
