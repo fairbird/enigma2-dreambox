4ba86a5d8391f4816188019127ff18bb3eceb3fc
fairbird <rrrr53@hotmail.com>
2023-10-30 20:40:51 +0300

    [FastChannelChange] Add missing import streamrelay

--- lib/python/Plugins/SystemPlugins/FastChannelChange/plugin.py
+++ lib/python/Plugins/SystemPlugins/FastChannelChange/plugin.py
@@ -13,6 +13,7 @@ from enigma import iPlayableService, eTimer, eServiceReference, iRecordableServi
 import os
 import glob
 from enigma import eFCCServiceManager
+from Screens.InfoBarGenerics import streamrelay
 
 g_max_fcc = len(glob.glob('/dev/fcc?'))
 g_default_fcc = (g_max_fcc) > 5 and 5 or g_max_fcc
