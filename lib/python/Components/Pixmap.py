OpenPLi/enigma2

Resolve issue when loading svg files for multipixmap
When loading svg files the size must be known and while loading the
pixmap the size is not known yet. Put when you actually select the png
the size becomes known. So I loaded the actual picture later.

lib/python/Components/Pixmap.py
@@ -95,9 +95,11 @@ class MovingPixmap(Pixmap):
 class MultiPixmap(Pixmap):
 	def __init__(self):
 		Pixmap.__init__(self)
-		self.pixmaps = []
+		self.pixmapfiles = []
+		self.pixmaps = {}
 
 	def applySkin(self, desktop, screen):
+		self.desktop = desktop
 		if self.skinAttributes is not None:
 			skin_path_prefix = getattr(screen, "skin_path", path)
 			pixmap = None
@@ -106,7 +108,8 @@ class MultiPixmap(Pixmap):
 				if attrib == "pixmaps":
 					pixmaps = value.split(',')
 					for p in pixmaps:
-						self.pixmaps.append(loadPixmap(resolveFilename(SCOPE_CURRENT_SKIN, p, path_prefix=skin_path_prefix), desktop))
+						self.pixmapfiles.append(resolveFilename(SCOPE_CURRENT_SKIN, p, path_prefix=skin_path_prefix))
+						#self.pixmaps.append(loadPixmap(resolveFilename(SCOPE_CURRENT_SKIN, p, path_prefix=skin_path_prefix), desktop))
 					if not pixmap:
 						pixmap = resolveFilename(SCOPE_CURRENT_SKIN, pixmaps[0], path_prefix=skin_path_prefix)
 				elif attrib == "pixmap":
@@ -120,7 +123,11 @@ class MultiPixmap(Pixmap):
 
 	def setPixmapNum(self, x):
 		if self.instance:
-			if len(self.pixmaps) > x:
+			if len(self.pixmapfiles) > x:
+				if not self.pixmaps:
+					(self.width, self.height) = self.getSize()
+				if x not in self.pixmaps:
+					self.pixmaps[x] = loadPixmap(self.pixmapfiles[x], self.desktop, self.width, self.height)
 				self.instance.setPixmap(self.pixmaps[x])
 			else:
 				print("setPixmapNum(%d) failed! defined pixmaps:" % (x), self.pixmaps)

fairbird/enigma2-dreambox

lib/python/Components/Pixmap.py
@@ -144,9 +144,11 @@ class MovingPixmap(Pixmap):
 class MultiPixmap(Pixmap):
 	def __init__(self):
 		Pixmap.__init__(self)
-		self.pixmaps = []
+		self.pixmapfiles = []
+		self.pixmaps = {}
 
 	def applySkin(self, desktop, screen):
+		self.desktop = desktop
 		if self.skinAttributes is not None:
 			myScreen, path = domScreens.get(screen.__class__.__name__, (None, None))
 			skinPathPrefix = getattr(screen, "skin_path", path)
@@ -163,7 +165,8 @@ class MultiPixmap(Pixmap):
 						else:
 							pngfile = ""
 						if pngfile and isfile(pngfile):
-							self.pixmaps.append(parsePixmap(pngfile, desktop))
+							self.pixmapfiles.append(parsePixmap(pngfile))
+							#self.pixmaps.append(parsePixmap(pngfile, desktop))
 					if not pixmap:
 						if fileExists(resolveFilename(SCOPE_GUISKIN, pixmaps[0], path_prefix=skinPathPrefix)):
 							pixmap = resolveFilename(SCOPE_GUISKIN, pixmaps[0], path_prefix=skinPathPrefix)
@@ -183,7 +186,11 @@ class MultiPixmap(Pixmap):
 
 	def setPixmapNum(self, index):
 		if self.instance:
-			if len(self.pixmaps) > index:
+			if len(self.pixmapfiles) > index:
+				if not self.pixmaps:
+					(self.width, self.height) = self.getSize()
+				if index not in self.pixmaps:
+					self.pixmaps[index] = loadPixmap(self.pixmapfiles[index], self.desktop, self.width, self.height)
 				self.instance.setPixmap(self.pixmaps[index])
 			else:
 				print("[Pixmap] setPixmapNum(%d) failed!  Defined pixmaps: %s." % (index, str(self.pixmaps)))