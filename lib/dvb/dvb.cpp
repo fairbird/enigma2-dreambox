086d3c354306f718915bbd607a7589c136ad8e4c
jbleyel <jbleyel@gmx.net>
2023-12-19 09:35:33 +0100

    eDVBUsbAdapter: add debug for vtuner init

--- lib/dvb/dvb.cpp
+++ lib/dvb/dvb.cpp
@@ -383,7 +383,11 @@ eDVBUsbAdapter::eDVBUsbAdapter(int nr)
 	while (vtunerFd < 0)
 	{
 		snprintf(filename, sizeof(filename), "/dev/misc/vtuner%d", vtunerid);
-		if (::access(filename, F_OK) < 0) break;
+		if (::access(filename, F_OK) < 0)
+		{
+			eDebug("[eDVBUsbAdapter] '%s' not found -> stop here!", filename);
+			break;
+		}
 		vtunerFd = open(filename, O_RDWR | O_CLOEXEC);
 		if (vtunerFd < 0)
 		{
