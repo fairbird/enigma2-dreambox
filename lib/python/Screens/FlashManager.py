cf1561494190995b805c5e2c2f49e561202bc723
fairbird <rrrr53@hotmail.com>
2024-02-03 19:58:40 +0300

    [FlashManager] Update

--- lib/python/Screens/FlashManager.py
+++ lib/python/Screens/FlashManager.py
@@ -63,7 +63,7 @@ class FlashManager(Screen, HelpableScreen):
 		Screen.__init__(self, session)
 		HelpableScreen.__init__(self)
 		self.skinName = ["FlashManager", "FlashOnline"]
-		self.imageFeed = "openATV"
+		self.imageFeed = "OpenPli"
 		self.setTitle(_("Flash Manager - %s Images") % self.imageFeed)
 		self.imagesList = {}
 		self.expanded = []
@@ -98,7 +98,7 @@ class FlashManager(Screen, HelpableScreen):
 		self["description"] = StaticText()
 		self["list"] = ChoiceList(list=[ChoiceEntryComponent("", ((_("Retrieving image list, please wait...")), "Loading"))])
 		self.feedUrls = [
-			("openATV", "https://images.mynonpublic.com/openatv/json/%s" % BoxInfo.getItem("BoxName"))
+			("Unknown", "https://Unknown.com")
 		]
 		self.callLater(self.getImagesList)
 
@@ -127,8 +127,8 @@ class FlashManager(Screen, HelpableScreen):
 					print("[FlashManager] getImagesList Error: Unable to extract file list from Zip file '%s'!" % file)
 
 		def getImagesListCallback(retVal=None):  # The retVal argument absorbs the unwanted return value from MessageBox.
-			if self.imageFeed != "openATV":
-				self.keyDistributionCallback("openATV")  # No images can be found for the selected distribution so go back to the openATV default.
+			if self.imageFeed != "Unknown":
+				self.keyDistributionCallback("Unknown")  # No images can be found for the selected distribution so go back to the Unknown default.
 
 		machinebuild = BoxInfo.getItem("machinebuild")
 		model = BoxInfo.getItem("model")
@@ -137,7 +137,7 @@ class FlashManager(Screen, HelpableScreen):
 		if not self.imagesList:
 			index = findInList(self.imageFeed)
 			box = machinebuild if index else boxname
-			feedURL = self.feedUrls[index][FEED_JSON_URL] if index else "https://images.mynonpublic.com/openatv/json/%s" % box
+			feedURL = self.feedUrls[index][FEED_JSON_URL] if index else "https://Unknown/json/%s" % box
 			try:
 				req = Request(feedURL, None, USER_AGENT)
 				self.imagesList = dict(load(urlopen(req)))
@@ -148,7 +148,7 @@ class FlashManager(Screen, HelpableScreen):
 				print("[FlashManager] getImagesList Error: Unable to load json data from URL '%s'!" % feedURL)
 				self.imagesList = {}
 			searchFolders = []
-			# Get all folders of /media/ and /media/net/ and only if openATV
+			# Get all folders of /media/ and /media/net/ and only if Unknown
 			if not index:
 				for media in ["/media/%s" % x for x in listdir("/media")] + (["/media/net/%s" % x for x in listdir("/media/net")] if isdir("/media/net") else []):
 					# print("[FlashManager] getImagesList DEBUG: media='%s'." % media)
@@ -188,7 +188,7 @@ class FlashManager(Screen, HelpableScreen):
 				self.setIndex = 0
 			self.selectionChanged()
 		else:
-			self.session.openWithCallback(getImagesListCallback, MessageBox, _("Error: Cannot find any images!"), type=MessageBox.TYPE_ERROR, timeout=3, windowTitle=self.getTitle())
+			self.session.openWithCallback(getImagesListCallback, MessageBox, _("Error: Cannot find any images!"), type=MessageBox.TYPE_ERROR, timeout=3, title=self.getTitle())
 
 	def keyCancel(self):
 		self.close()
@@ -236,12 +236,12 @@ class FlashManager(Screen, HelpableScreen):
 		self.selectionChanged()
 
 	def keyDistribution(self):
-		self.feedUrls = [["openATV", "https://images.mynonpublic.com/openatv/json/%s" % BoxInfo.getItem("BoxName")]]
+		self.feedUrls = [["Unknown", "https://Unknown/json/%s" % BoxInfo.getItem("BoxName")]]
 		distributionList = []
 		default = 0
 		machine = BoxInfo.getItem("machinebuild")
 		try:
-			req = Request("https://raw.githubusercontent.com/OpenATV/FlashImage/gh-pages/%s.json" % machine, None, USER_AGENT)
+			req = Request("https://Unknown/%s.json" % machine, None, USER_AGENT)
 			responseList = load(urlopen(req, timeout=5))
 			self.feedUrls = self.feedUrls + responseList
 		except Exception as err:
@@ -250,12 +250,12 @@ class FlashManager(Screen, HelpableScreen):
 			distributionList.append((distribution, distribution))
 			if distribution == self.imageFeed:
 				default = index
-		self.session.openWithCallback(self.keyDistributionCallback, MessageBox, _("Please select a distribution from which you would like to flash an image:"), list=distributionList, default=default, windowTitle=_("Flash Manager - Distributions"))
+		self.session.openWithCallback(self.keyDistributionCallback, MessageBox, _("Please select a distribution from which you would like to flash an image:"), list=distributionList, default=default, title=_("Flash Manager - Distributions"))
 
 	def keyDistributionCallback(self, distribution):
 		if distribution:
 			self.imageFeed = distribution
-			# TRANSLATORS: The variable is the name of a distribution.  E.g. "openATV".
+			# TRANSLATORS: The variable is the name of a distribution.  E.g. "Unknown".
 			self.setTitle(_("Flash Manager - %s Images") % self.imageFeed)
 			self.imagesList = {}
 			self.expanded = []
@@ -276,7 +276,7 @@ class FlashManager(Screen, HelpableScreen):
 					self.imagesList = {}
 					self.getImagesList()
 				except OSError as err:
-					self.session.open(MessageBox, _("Error %d: Unable to delete downloaded image '%s'!  (%s)" % (err.errno, currentSelection, err.strerror)), MessageBox.TYPE_ERROR, timeout=3, windowTitle=self.getTitle())
+					self.session.open(MessageBox, _("Error %d: Unable to delete downloaded image '%s'!  (%s)" % (err.errno, currentSelection, err.strerror)), MessageBox.TYPE_ERROR, timeout=3, title=self.getTitle())
 
 		currentSelectionImage = self["list"].getCurrent()[0][0]
 		self.session.openWithCallback(keyDeleteImageCallback, MessageBox, _("Do you really want to delete '%s'?") % currentSelectionImage, MessageBox.TYPE_YESNO, default=False)
@@ -390,7 +390,7 @@ class FlashImage(Screen, HelpableScreen):
 				default = index
 			choices.append((slotMsg % (slotCode, slotType, imageDictionary[slotCode]["imagename"], current), (slotCode, True)))
 		choices.append((_("No, don't flash this image"), False))
-		self.session.openWithCallback(self.checkMedia, MessageBox, _("Do you want to flash the image '%s'?") % self.imageName, list=choices, default=default, windowTitle=self.getTitle())
+		self.session.openWithCallback(self.checkMedia, MessageBox, _("Do you want to flash the image '%s'?") % self.imageName, list=choices, default=default, title=self.getTitle())
 
 	def checkMedia(self, choice):
 		if choice:
@@ -427,7 +427,7 @@ class FlashImage(Screen, HelpableScreen):
 				if MultiBoot.canMultiBoot():
 					self.slotCode = choice[0]
 				if BoxInfo.getItem("distro") in self.imageName:
-					self.session.openWithCallback(self.backupQuestionCallback, MessageBox, _("Do you want to backup settings?"), default=True, timeout=10, windowTitle=self.getTitle())
+					self.session.openWithCallback(self.backupQuestionCallback, MessageBox, _("Do you want to backup settings?"), default=True, timeout=10, title=self.getTitle())
 				else:
 					self.backupQuestionCallback(None)
 				return
@@ -446,11 +446,11 @@ class FlashImage(Screen, HelpableScreen):
 					elif isDevice or "no_backup" == choice:
 						self.startBackupSettings(choice)
 					else:
-						self.session.openWithCallback(self.startBackupSettings, MessageBox, _("Warning: There is only a network drive to store the backup. This means the auto restore will not work after the flash. Alternatively, mount the network drive after the flash and perform a manufacturer reset to auto restore."), windowTitle=self.getTitle())
+						self.session.openWithCallback(self.startBackupSettings, MessageBox, _("Warning: There is only a network drive to store the backup. This means the auto restore will not work after the flash. Alternatively, mount the network drive after the flash and perform a manufacturer reset to auto restore."), title=self.getTitle())
 				except OSError as err:
-					self.session.openWithCallback(self.keyCancel, MessageBox, _("Error: Unable to create the required directories on the target device (e.g. USB stick or hard disk)! Please verify device and try again."), type=MessageBox.TYPE_ERROR, windowTitle=self.getTitle())
+					self.session.openWithCallback(self.keyCancel, MessageBox, _("Error: Unable to create the required directories on the target device (e.g. USB stick or hard disk)! Please verify device and try again."), type=MessageBox.TYPE_ERROR, title=self.getTitle())
 			else:
-				self.session.openWithCallback(self.keyCancel, MessageBox, _("Error: Could not find a suitable device! Please remove some downloaded images or attach another device (e.g. USB stick) with sufficient free space and try again."), type=MessageBox.TYPE_ERROR, windowTitle=self.getTitle())
+				self.session.openWithCallback(self.keyCancel, MessageBox, _("Error: Could not find a suitable device! Please remove some downloaded images or attach another device (e.g. USB stick) with sufficient free space and try again."), type=MessageBox.TYPE_ERROR, title=self.getTitle())
 		else:
 			self.keyCancel()
 
@@ -497,7 +497,7 @@ class FlashImage(Screen, HelpableScreen):
 					(_("Do not flash image"), "abort")
 				]
 				default = 0
-			self.session.openWithCallback(self.postFlashActionCallback, MessageBox, text, list=choices, default=default, windowTitle=self.getTitle())
+			self.session.openWithCallback(self.postFlashActionCallback, MessageBox, text, list=choices, default=default, title=self.getTitle())
 		else:
 			self.keyCancel()
 
@@ -520,7 +520,7 @@ class FlashImage(Screen, HelpableScreen):
 				nextRecordingTime = self.session.nav.RecordTimer.getNextRecordingTime()
 				if recording or (nextRecordingTime > 0 and (nextRecordingTime - time()) < 360):
 					self.choice = choice
-					self.session.openWithCallback(self.recordWarning, MessageBox, "%s\n\n%s" % (_("Recording(s) are in progress or coming up in few seconds!"), _("Flash your %s %s and reboot now?") % getBoxDisplayName()), default=False, windowTitle=self.getTitle())
+					self.session.openWithCallback(self.recordWarning, MessageBox, "%s\n\n%s" % (_("Recording(s) are in progress or coming up in few seconds!"), _("Flash your %s %s and reboot now?") % getBoxDisplayName()), default=False, title=self.getTitle())
 					return
 			restoreSettings = ("restoresettings" in choice)
 			restoreSettingsnoPlugin = (choice == "restoresettingsnoplugin")
@@ -608,7 +608,7 @@ class FlashImage(Screen, HelpableScreen):
 
 	def downloadError(self, error):
 		self.downloader.stop()
-		self.session.openWithCallback(self.keyCancel, MessageBox, "%s\n\n%s" % (_("Error downloading image '%s'!") % self.imageName, error.strerror), type=MessageBox.TYPE_ERROR, windowTitle=self.getTitle())
+		self.session.openWithCallback(self.keyCancel, MessageBox, "%s\n\n%s" % (_("Error downloading image '%s'!") % self.imageName, error.strerror), type=MessageBox.TYPE_ERROR, title=self.getTitle())
 
 	def unzip(self):
 		self["header"].setText(_("Unzipping Image"))
@@ -627,7 +627,7 @@ class FlashImage(Screen, HelpableScreen):
 			self.flashImage()
 		except Exception as err:
 			print("[FlashManager] startUnzip Error: %s!" % str(err))
-			self.session.openWithCallback(self.keyCancel, MessageBox, _("Error unzipping image '%s'!") % self.imageName, type=MessageBox.TYPE_ERROR, windowTitle=self.getTitle())
+			self.session.openWithCallback(self.keyCancel, MessageBox, _("Error unzipping image '%s'!") % self.imageName, type=MessageBox.TYPE_ERROR, title=self.getTitle())
 
 	def flashImage(self):
 		def findImageFiles(path):
@@ -675,7 +675,7 @@ class FlashImage(Screen, HelpableScreen):
 			self.containerOFGWrite.ePopen([OFGWRITE, OFGWRITE] + cmdArgs + ['%s' % imageFiles], callback=self.flashImageDone)
 			fbClass.getInstance().lock()
 		else:
-			self.session.openWithCallback(self.keyCancel, MessageBox, _("Error: Image '%s' to install is invalid!") % self.imageName, type=MessageBox.TYPE_ERROR, windowTitle=self.getTitle())
+			self.session.openWithCallback(self.keyCancel, MessageBox, _("Error: Image '%s' to install is invalid!") % self.imageName, type=MessageBox.TYPE_ERROR, title=self.getTitle())
 
 	def flashImageDone(self, data, retVal, extraArgs):
 		fbClass.getInstance().unlock()
@@ -685,4 +685,4 @@ class FlashImage(Screen, HelpableScreen):
 			self["summary_header"].setText(self["header"].getText())
 			self["info"].setText("%s\n\n%s\n%s" % (self.imageName, _("Press OK for MultiBoot selection."), _("Press EXIT to close.")))
 		else:
-			self.session.openWithCallback(self.keyCancel, MessageBox, _("Flashing image '%s' was not successful!") % self.imageName, type=MessageBox.TYPE_ERROR, windowTitle=self.getTitle())
+			self.session.openWithCallback(self.keyCancel, MessageBox, _("Flashing image '%s' was not successful!") % self.imageName, type=MessageBox.TYPE_ERROR, title=self.getTitle())
