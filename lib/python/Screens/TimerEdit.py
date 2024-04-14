f8140ae30b05c596fe2913bc48ed954b16965018
Littlesat <littlesat@openpli.org>
2024-04-14 11:50:35 +0200

    With timer edit when timer was local timer keep it as local timer
    When on a client box you programmed a timer as local timer and you had
    fallback timers enabled then when editing such existing time the
    default was set back to the existing default. With this change only
    'new' timers are adapted to the defined default

--- lib/python/Screens/TimerEdit.py
+++ lib/python/Screens/TimerEdit.py
@@ -349,7 +349,7 @@ class TimerEditList(Screen):
 		self.addTimer(timer)
 
 	def addTimer(self, timer):
-		self.session.openWithCallback(self.finishedAdd, TimerEntry, timer)
+		self.session.openWithCallback(self.finishedAdd, TimerEntry, timer, newEntry=True)
 
 	def finishedEdit(self, answer):
 		print("[TimerEditList] finished edit")
