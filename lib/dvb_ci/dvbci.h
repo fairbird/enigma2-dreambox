aca1bed036d3576b70021e107a9f0ea524447181
Dimitrij <Dima-73@inbox.lv>
2024-05-17 11:52:05 +0300

    Possibility to disable operator profiles for CI+ module (#3983)
    thanks @DimitarCC

--- lib/dvb_ci/dvbci.h
+++ lib/dvb_ci/dvbci.h
@@ -78,6 +78,7 @@ class eDVBCISlot: public iObject, public sigc::trackable
 	int m_tunernum;
 	eMainloop *m_context;
 	int m_ciplus_routing_tunernum;
+	bool m_operator_profiles_disabled;
 	std::string m_ciplus_routing_input;
 	std::string m_ciplus_routing_ci_input;
 
@@ -131,6 +132,7 @@ public:
 	int getSlotID();
 	int getNumOfServices();
 	int getVersion();
+	bool getIsOperatorProfileDisabled() { return m_operator_profiles_disabled; };
 	int16_t getCADemuxID() { return m_ca_demux_id; };
 	int getTunerNum() { return m_tunernum; };
 	int getUseCount() { return use_count; };
