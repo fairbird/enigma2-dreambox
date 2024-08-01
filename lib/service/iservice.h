bda3fffaaa8e5b160e94b04d73dab67db6372a6b
fairbird <rrrr53@hotmail.com>
2024-08-01 14:35:02 +0300

    [eSubtitleSettings] update

--- lib/service/iservice.h
+++ lib/service/iservice.h
@@ -706,7 +706,6 @@ class PyList;
 struct eDVBTeletextSubtitlePage;
 struct eDVBSubtitlePage;
 struct ePangoSubtitlePage;
-struct eVobSubtitlePage;
 class eRect;
 class gRegion;
 class gPixmap;
@@ -718,7 +717,6 @@ public:
 	virtual void setPage(const eDVBTeletextSubtitlePage &p) = 0;
 	virtual void setPage(const eDVBSubtitlePage &p) = 0;
 	virtual void setPage(const ePangoSubtitlePage &p) = 0;
-	virtual void setPage(const eVobSubtitlePage &p) = 0;
 	virtual void setPixmap(ePtr<gPixmap> &pixmap, gRegion changed, eRect dest) = 0;
 	virtual void destroy() = 0;
 };
