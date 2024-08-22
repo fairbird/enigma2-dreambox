fc26142b9a21f74e0da740251707f2ede5b3e318
Littlesat <littlesat@openpli.org>
2024-08-22 08:14:12 +0200

    Avoid GSOD when errors occurred in the output of the Console

--- lib/python/Screens/Console.py
+++ lib/python/Screens/Console.py
@@ -118,5 +118,5 @@ class Console(Screen):
 
 	def dataAvail(self, data):
 		if isinstance(data, bytes):
-			data = data.decode()
+			data = data.decode(errors='ignore')
 		self["text"].appendText(data)
