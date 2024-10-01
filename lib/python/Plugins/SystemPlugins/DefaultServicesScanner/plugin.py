07414bd9b3512de97e4a0c4f0a2dd6d0f7866581
jbleyel <jbleyel@gmx.net>
2024-10-01 12:00:48 +0200

    "[DefaultServicesScanner]" fix typo

--- lib/python/Plugins/SystemPlugins/DefaultServicesScanner/plugin.py
+++ lib/python/Plugins/SystemPlugins/DefaultServicesScanner/plugin.py
@@ -44,7 +44,7 @@ class DefaultServiceScan(ServiceScan):
 		db.reloadServicelist()
 		ServiceScan.__init__(self, session, scanList)
 		self.timer = eTimer()
-		self.timer.callback.append(self.ok)
+		self.timer.callback.append(self.keySave)
 		self.timer.start(1000)
 
 
