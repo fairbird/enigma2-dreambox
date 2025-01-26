fe336f253536ecb1b7bea04cf9d6c34ce50b8d6e
fairbird <rrrr53@hotmail.com>
2025-01-26 17:55:19 +0300

    "[InfoBar]" Update External subtitles
    cherry-pick > https://github.com/OpenPLi/enigma2/commit/6d36942ab79422a4b14725e4b794a9de12fcb379

--- lib/python/Screens/InfoBar.py
+++ lib/python/Screens/InfoBar.py
@@ -282,14 +282,17 @@ class MoviePlayer(InfoBarBase, InfoBarShowHide, InfoBarMenu, InfoBarSeek, InfoBa
 		self.servicelist = slist
 		self.infobar = infobar
 		self.lastservice = lastservice or session.nav.getCurrentlyPlayingServiceOrGroup()
-		path = splitext(service.getPath())[0]
+
+		path = service and service.getPath() and splitext(service.getPath())[0] or ""
 		subs = []
-		for sub in ("srt", "ass", "ssa"):
-			subs = glob("%s*.%s" % (path, sub))
-			if subs:
-				break
+		if path:
+			for sub in ("srt", "ass", "ssa"):
+				subs = glob("%s*.%s" % (path, sub))
+				if subs:
+					break
 		if subs:
 			service.setSubUri(subs[0])  # Support currently only one external sub
+
 		session.nav.playService(service)
 		self.cur_service = service
 		self.returning = False
