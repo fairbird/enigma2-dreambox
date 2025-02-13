b3e7c7bb26a47ed05a8c29cceb34c93d1dd83491
Littlesat <littlesat@openpli.org>
2025-02-13 16:57:42 +0100

    For insert service when we have HDMI-in do not remove normal service

--- lib/python/Screens/ChannelSelection.py
+++ lib/python/Screens/ChannelSelection.py
@@ -69,7 +69,7 @@ class InsertService(Setup):
 	def createConfig(self):
 		choices = [("Select Service", _("Select Service"))]
 		if BoxInfo.getItem("HasHDMIin"):
-			choices = [("HDMI-in", _("HDMI-In"))]
+			choices.append(("HDMI-in", _("HDMI-In")))
 		choices.append(("IPTV stream", _("Enter URL")))
 		self.servicetype = ConfigSelection(choices=choices)
 		self.streamtype = ConfigSelection(["1", "4097", "5001", "5002"])
