4b872491ad2b054dcafb5843d66119796bc4d1c6
fairbird <rrrr53@hotmail.com>
2025-02-26 20:07:43 +0300

    Update

--- lib/service/servicedvb.cpp
+++ lib/service/servicedvb.cpp
@@ -536,7 +536,7 @@ RESULT eStaticServiceDVBPVRInformation::getEvent(const eServiceReference &ref, e
 			std::string filename = ref.path;
 			filename.erase(filename.length()-2, 2);
 			filename+="eit";
-			if (!event->parseFrom(filename, (m_parser.m_ref.getTransportStreamID().get()<<16)|m_parser.m_ref.getOriginalNetworkID().get()))
+			if (!event->parseFrom(filename, (m_parser.m_ref.getTransportStreamID().get()<<16)|m_parser.m_ref.getOriginalNetworkID().get(), m_parser.m_ref.getServiceID().get()))
 			{
 				evt = event;
 				return 0;
