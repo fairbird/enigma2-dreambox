d0e2156b4d6b945712e5ee8ef87df0002272356f
Littlesat <littlesat@openpli.org>
2025-03-12 18:54:25 +0100

    Resolve removal of debuglog files in troubleshoot screen
    The removal of log files was based on the 'cat filename.log' string
    where we removed the cat. But debug files do 'tail -n 2500
    filename.debuglog". When we remove the cat it will become ' -n 2500
    filename.debuglog" as filename as that does not work. So I hope it is
    sufficiant to resolve it by stripping with rstrip(" ', 1)[1]. For all
    log files this should be sufficiant.

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -423,7 +423,7 @@ class Troubleshoot(Screen):
 	def green(self):
 		if self.commandIndex >= self.numberOfCommands:
 			try:
-				os.remove(self.commands[self.commandIndex][4:])
+				os.remove(self.commands[self.commandIndex].rsplit(" ", 1)[1])
 			except:
 				pass
 			self.updateOptions()
@@ -431,7 +431,7 @@ class Troubleshoot(Screen):
 
 	def removeAllLogfiles(self, answer):
 		if answer:
-			for fileName in self.getLogFilesList():
+			for fileName in self.getLogFilesList() + self.getDebugFilesList():
 				try:
 					os.remove(fileName)
 				except:
