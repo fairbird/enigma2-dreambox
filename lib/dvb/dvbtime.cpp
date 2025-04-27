c3b84e049635f7351465eb018d078f0f101bbf2a
fairbird <rrrr53@hotmail.com>
2025-04-27 11:15:19 +0300

    Fix build with largefile and 64bit time_t support systemwide https://git.openembedded.org/openembedded-core/commit/meta/conf?h=nanbield&id=b9e0c5e750c3097e176fdc18b3b58b622f716e71
    /../git/lib/base/eerror.cpp:176:63: error: format '%lu' expects argument of type 'long unsigned int', but argument 4 has type '__time64_t' {aka 'long long int'} [-Werror=format=]
    |   176 |                         pos = snprintf(buf, pos + vsize, '<%6lu.%03lu> ', tp.tv_sec, tp.tv_nsec/1000000);
    |       |                                                            ~~~^           ~~~~~~~~~
    |       |                                                               |              |
    |       |                                                               |              __time64_t {aka long long int}
    |       |                                                               long unsigned int
    |       |                                                            %6llu

--- lib/dvb/dvbtime.cpp
+++ lib/dvb/dvbtime.cpp
@@ -368,7 +368,7 @@ void eDVBLocalTimeHandler::updateTime(time_t tp_time, eDVBChannel *chan, int upd
 {
 
 	if (m_time_debug)
-		eDebug("[eDVBLocalTimerHandler] updateTime : %ld", tp_time);
+		eDebug("[eDVBLocalTimerHandler] updateTime : %lld", tp_time);
 
 	if (m_SyncTimeUsing == 2)
 	{
@@ -458,7 +458,7 @@ void eDVBLocalTimeHandler::updateTime(time_t tp_time, eDVBChannel *chan, int upd
 						   now.tm_sec);
 				time_difference = rtc_time - linuxTime;
 				if (m_time_debug)
-					eDebug("[eDVBLocalTimerHandler] RTC to receiver time difference is %ld seconds.", linuxTime - rtc_time);
+					eDebug("[eDVBLocalTimerHandler] RTC to receiver time difference is %lld seconds.", linuxTime - rtc_time);
 				if (time_difference)
 				{
 					if (m_time_debug)
@@ -541,7 +541,7 @@ void eDVBLocalTimeHandler::updateTime(time_t tp_time, eDVBChannel *chan, int upd
 					m_timeOffsetMap[chan->getChannelID()] = rtc - tp_time;
 					new_diff = rtc - linuxTime; // Set enigma time to RTC.
 					if (m_time_debug)
-						eDebug("[eDVBLocalTimerHandler] Update stored correction to %ld.  (Calculated against RTC time.)", rtc - tp_time);
+						eDebug("[eDVBLocalTimerHandler] Update stored correction to %lld.  (Calculated against RTC time.)", rtc - tp_time);
 				}
 				else if (abs(ddiff) <= 120)
 				{
