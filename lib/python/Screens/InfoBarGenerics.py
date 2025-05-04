726a6fe8a3faaa273aa1fa4610360e3eb2f497c6
Littlesat <littlesat@openpli.org>
2025-05-04 11:46:40 +0200

    Try to bring back seek for Video On Demand in Bouquets in a better way
    Standard the numbers are associated to numberzap but with long-exit
    button in a seekable service you can from now on toggle between
    numberzap and seek stepskip when a service is e.g. seekable. After a zap
    it defaults back always to number zap. On the screen you also see
    shortly the status if Numberbutton is zap or in seekmode. For 'normal'
    services the long exit button does nothing.

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -833,7 +833,11 @@ class InfoBarNumberZap:
 	""" Handles an initial number for NumberZapping """
 
 	def __init__(self):
-		self["NumberActions"] = NumberActionMap(["NumberActions"],
+		self.__event_tracker = ServiceEventTracker(screen=self, eventmap={
+				iPlayableService.evStart: self.__serviceStarted,
+			})
+		self.toggleSeekStatus = False
+		self["NumberActions"] = NumberActionMap(["NumberActions", "InfobarSeekActions"],
 			{
 				"1": self.keyNumberGlobal,
 				"2": self.keyNumberGlobal,
@@ -845,7 +849,17 @@ class InfoBarNumberZap:
 				"8": self.keyNumberGlobal,
 				"9": self.keyNumberGlobal,
 				"0": self.keyNumberGlobal,
+				"toggleSeek": self.toggleSeek,
 			})
+
+	def __serviceStarted(self):
+		self.toggleSeekStatus = False
+
+	def toggleSeek(self):
+		self.seekable = self.getSeek()
+		if self.seekable:
+			self.toggleSeekStatus = not self.toggleSeekStatus
+			self.VideoMode_window.setText(_("Numberbuttons Seek") if self.toggleSeekStatus else _("Numberbuttons Zap"))
 
 	def keyNumberGlobal(self, number):
 		if number == 0:
@@ -854,6 +868,15 @@ class InfoBarNumberZap:
 			elif len(self.servicelist.history) > 1:
 				self.checkTimeshiftRunning(self.recallPrevService)
 		else:
+			if self.toggleSeekStatus:
+				length = self.seekable.getLength() or (None, 0)
+				if length[1] > 0:
+					key = int(number)
+					time = (-config.seek.selfdefined_13.value, False, config.seek.selfdefined_13.value,
+						-config.seek.selfdefined_46.value, False, config.seek.selfdefined_46.value,
+						-config.seek.selfdefined_79.value, False, config.seek.selfdefined_79.value)[key - 1]
+					self.seekable.seekRelative(time < 0 and -1 or 1, abs(time * 90000))
+				return
 			if "TimeshiftActions" in self and self.timeshiftEnabled():
 				ts = self.getTimeshift()
 				if ts and ts.isTimeshiftActive():
