8c61862c527f0c06caa95550b79bc7ad2958f1fd
fairbird <rrrr53@hotmail.com>
2025-05-17 02:36:52 +0300

    [RecordTimer] fix duplicate notifications cherry-pick > https://github.com/openatv/enigma2/commit/0ebbe7c67589ca037e2d48897fc2d992e033d176

--- lib/python/RecordTimer.py
+++ lib/python/RecordTimer.py
@@ -773,8 +773,8 @@ class RecordTimerEntry(timer.TimerEntry):
 
 	def gotRecordEvent(self, record, event):
 		# TODO: this is not working (never true), please fix. (comparing two swig wrapped ePtrs)
-		#if self.__record_service.__deref__() != record.__deref__():
-		#	return
+		if self.__record_service.__deref__() != record.__deref__():
+			return
 		#self.log(16, "record event %d" % event)
 		if event == iRecordableService.evRecordWriteError:
 			print("WRITE ERROR on recording, disk full?")
