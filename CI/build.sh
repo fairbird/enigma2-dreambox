400e1d418fc92bbc47947e40473d999810101706
Atef <31848885+atefganm@users.noreply.github.com>
2025-05-19 02:23:23 +0300

    fix actions/setup-python@v5 and Source url (#91)

--- CI/build.sh
+++ CI/build.sh
@@ -20,7 +20,7 @@ commit_files() {
 }
 
 upload_files() {
-  git remote add upstream https://${GITHUB_TOKEN}@github.com/atefganm/enigma2-dreambox.git > /dev/null 2>&1
+  git remote add upstream https://${GITHUB_TOKEN}@github.com/fairbird/enigma2-dreambox.git > /dev/null 2>&1
   git push --quiet upstream test || echo "failed to push with error $?"
 }
 
