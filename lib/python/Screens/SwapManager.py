15dbb241a0500c3c6070cc75cb90ae79eb15d55b
jbleyel <jbleyel@gmx.net>
2025-05-30 11:23:42 +0200

    "[Swapmanager]" remove six

--- lib/python/Screens/SwapManager.py
+++ lib/python/Screens/SwapManager.py
@@ -13,7 +13,6 @@ from os import system, stat as mystat, path, remove, rename
 from enigma import eTimer, getDesktop
 from glob import glob
 import stat
-import six
 
 config.usage.swapautostart = ConfigYesNo(default=False)
 
@@ -47,7 +46,8 @@ class StartSwap:
 
 	def startSwap2(self, result=None, retval=None, extra_args=None):
 		if result is not None:
-			result = six.ensure_str(result)
+			if isinstance(result, bytes):
+				result = result.decode(encoding='utf-8', errors='strict')
 		swap_place = ""
 		if result and result.find('sd') != -1:
 			for line in result.split('\n'):
@@ -181,7 +181,8 @@ class Swap(Screen):
 
 	def updateSwap2(self, result=None, retval=None, extra_args=None):
 		if result is not None:
-			result = six.ensure_str(result)
+			if isinstance(result, bytes):
+				result = result.decode(encoding='utf-8', errors='strict')
 		self.swapsize = 0
 		self.swap_place = ''
 		self.swap_active = False
