21b84636054b3bd594b1947f107e142f8d8029ca
fairbird <rrrr53@hotmail.com>
2025-07-13 22:52:33 +0300

    update

--- lib/gdi/picload.cpp
+++ lib/gdi/picload.cpp
@@ -408,7 +408,6 @@ static void png_load(Cfilepara* filepara, uint32_t background, bool forceRGB = f
 
 	// When we have indexed (8bit) PNG convert it to standard 32bit png so to preserve transparency and to allow proper alphablending
 	if (color_type == PNG_COLOR_TYPE_PALETTE && bit_depth == 8) {
-		color_type = PNG_COLOR_TYPE_RGBA;
 		eTrace("[ePicLoad] Interlaced PNG 8bit -> 32bit");
 	}
 
