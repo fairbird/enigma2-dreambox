e091569cca3ad8e4cba764a0994eeebf504adcf3
fairbird <rrrr53@hotmail.com>
2025-07-15 11:40:19 +0300

    [Information] * get rust version and upx version from enigma.info
    cherry-pick > https://github.com/openatv/enigma2/commit/d036ee2b3c17e447444bbb7c3596e773abfff5db

--- lib/python/Components/About.py
+++ lib/python/Components/About.py
@@ -268,23 +268,6 @@ def getDriverInstalledDate():
 	return result if result else _("Unknown")
 
 
-def getFileCompressionInfo():
-	try:
-		with open("/bin/bash", "rb") as fd:
-			fd.seek(399000)
-			content = fd.read(1000)
-		# Search for something like "Id: UPX 4.24 Copyright (C) 1996-2024 the UPX Team. All Rights Reserved." in the binary content
-		match = search(rb"Id: UPX.*Copyright", content)
-		if match:
-			parts = match.group(0).decode("UTF-8").strip().split()
-			result = f"{_("Enabled")} ({parts[1].lower()} {parts[2]})" if len(parts) >= 3 else _("Disabled")
-		else:
-			result = _("Disabled")
-	except Exception:
-		result = _("Disabled")
-	return result
-
-
 def getVersionFromOpkg(fileName):
 	return next((line[9:].split("+")[0] for line in fileReadLines(f"/var/lib/opkg/info/{fileName}.control", default=[], source=MODULE_NAME) if line.startswith("Version:")), _("Not Installed"))
 
@@ -363,14 +346,6 @@ def getPythonVersionString():
 	try:
 		result = pyversion.split(" ")[0]
 	except Exception:
-		result = _("Unknown")
-	return result
-
-
-def getRustVersion():
-	try:
-		from bcrypt import _rustVersion_ as result
-	except ImportError:
 		result = _("Unknown")
 	return result
 
--- lib/python/Screens/Information.py
+++ lib/python/Screens/Information.py
@@ -715,7 +715,8 @@ class DistributionInformation(InformationBase):
 			info.append(formatLine("P1", _("Distribution folder"), BoxInfo.getItem("imagedir")))
 		if BoxInfo.getItem("imagefs"):
 			info.append(formatLine("P1", _("Distribution file system"), BoxInfo.getItem("imagefs").strip()))
-		info.append(formatLine("P1", _("File compression"), about.getFileCompressionInfo()))
+		upxVersion = BoxInfo.getItem("upx")
+		info.append(formatLine("P1", _("File compression"), f"{_("Enabled")} (upx {upxVersion})" if upxVersion else _("Disabled")))
 		info.append(formatLine("P1", _("Feed URL"), BoxInfo.getItem("feedsurl")))
 		info.append(formatLine("P1", _("Compiled by"), BoxInfo.getItem("developername")))
 		info.append("")
@@ -726,7 +727,7 @@ class DistributionInformation(InformationBase):
 		info.append(formatLine("P1", _("Glibc version"), about.getGlibcVersion()))
 		info.append(formatLine("P1", _("OpenSSL version"), about.getVersionFromOpkg("openssl")))
 		info.append(formatLine("P1", _("Python version"), about.getPythonVersionString()))
-		info.append(formatLine("P1", _("Rust version"), about.getRustVersion()))
+		info.append(formatLine("P1", _("Rust version"), BoxInfo.getItem("rust")))
 		info.append(formatLine("P1", _("GStreamer version"), getGStreamerVersionString().replace("GStreamer ", "")))
 		info.append(formatLine("P1", _("FFmpeg version"), about.getVersionFromOpkg("ffmpeg")))
 		bootId = fileReadLine("/proc/sys/kernel/random/boot_id", source=MODULE_NAME)
