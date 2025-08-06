9a6fcbd880b0e871d8f89b3e803de311ababd7f3
fairbird <rrrr53@hotmail.com>
2025-08-06 11:37:29 +0300

    [SimpleSummary] Update based on OpenATV

--- lib/python/Screens/SimpleSummary.py
+++ lib/python/Screens/SimpleSummary.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from Components.Sources.StaticText import StaticText
 from Screens.Screen import Screen
 
 
@@ -9,16 +8,16 @@ class SimpleSummary(Screen):
 		<widget source="global.CurrentTime" render="Label" position="56,46" size="82,18" font="Regular;16">
 			<convert type="ClockToText">WithSeconds</convert>
 		</widget>
-		<widget source="Title" render="Label" position="6,4" size="120,42" font="Regular;18" />
+		<widget source="parent.Title" render="Label" position="6,4" size="120,42" font="Regular;18" />
 	</screen>"""
 
 	def __init__(self, session, parent):
 		Screen.__init__(self, session, parent=parent)
-		self["Title"] = StaticText(parent.getTitle())
 		names = parent.skinName
 		if not isinstance(names, list):
 			names = [names]
-		self.skinName = ["%s_summary" % x for x in names]
+		self.skinName = ["%sSummary" % x for x in names]
+		self.skinName += ["%s_summary" % x for x in names]
 		self.skinName.append("SimpleSummary")
 		# If parent has a "skin_summary" defined, use that as default.
 		self.skin = parent.__dict__.get("skin_summary", self.skin)
