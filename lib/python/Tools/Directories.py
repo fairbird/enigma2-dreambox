62a8542d361b1a16f212b22a5c3a02a9508c704c
IanSav <6904736+IanSav@users.noreply.github.com>
2025-08-07 23:44:08 +1000

    "[Directories.py]" Simplify ResolveLists class
    The methods in this class are not used in OpenATV.

--- lib/python/Tools/Directories.py
+++ lib/python/Tools/Directories.py
@@ -108,14 +108,6 @@ class ResolveLists:
 	lcdSkin = []
 	fonts = []
 
-	def __init__(self):
-		pass
-
-	def clearResolveLists(self):
-		skin.clear()
-		lcdSkin.clear()
-		fonts.clear()
-
 
 def resolveFilename(scope, base="", path_prefix=None):
 	def addIfExists(paths):
