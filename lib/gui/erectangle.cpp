768c70fd8915ae3e67c6fcc7b973af0f924ba542
fairbird <rrrr53@hotmail.com>
2025-09-28 02:00:18 +0300

    [erectangle] Fix

--- lib/gui/erectangle.cpp
+++ lib/gui/erectangle.cpp
@@ -2,9 +2,4 @@
 #include <lib/gui/erectangle.h>
 #include <lib/gui/ewidgetdesktop.h>
 
-eRectangle::eRectangle(eWidget *parent)
-	: eWidget(parent)
-{
-}
-
 eRectangle::eRectangle(eWidget* parent) : eWidget(parent) {}
