907dfe5ef87a04f59785823f5a596d98551d1cf1
fairbird <rrrr53@hotmail.com>
2025-09-30 17:25:41 +0000

    PEP8 double aggressive E225 ~ E228 and E231

--- lib/python/Components/Sources/List.py
+++ lib/python/Components/Sources/List.py
@@ -27,9 +27,9 @@ to generate HTML."""
 		self.onSelectionChanged = []
 		self.onListUpdated = []
 		self.disableCallbacks = False		
-		self.__current = None # current element set from connected GUI element
-		self.__index = None # current index set from connected GUI element
-		self.connectedGuiElement = None # manuallyconnected GUI element
+		self.__current = None  # current element set from connected GUI element
+		self.__index = None  # current index set from connected GUI element
+		self.connectedGuiElement = None  # manuallyconnected GUI element
 
 	def enableAutoNavigation(self, enabled):
 		try:
