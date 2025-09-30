84798dac94adebd2798cb43ad513721af2e78d94
fairbird <rrrr53@hotmail.com>
2025-09-30 20:16:24 +0300

    [epng] update scale and cached options for loadSVG and loadImage cherry-pick > https://github.com/openatv/enigma2/commit/722a45e957b6c9057d6d41f0176d035b622b3c64

--- lib/gdi/epng.cpp
+++ lib/gdi/epng.cpp
@@ -7,6 +7,7 @@
 #include <lib/gdi/epng.h>
 #include <lib/gdi/pixmapcache.h>
 #include <unistd.h>
+#include <lib/base/estring.h>
 
 #include <map>
 #include <string>
@@ -544,16 +545,16 @@ int loadSVG(ePtr<gPixmap> &result, const char *filename, int cached, int width, 
 	return 0;
 }
 
-int loadImage(ePtr<gPixmap> &result, const char *filename, int accel, int width, int height)
+int loadImage(ePtr<gPixmap> &result, const char *filename, int accel, int width, int height, int cached, float scale, int keepAspect, int align)
 {
 	if (endsWith(filename, ".png"))
-		return loadPNG(result, filename, accel, 1);
+		return loadPNG(result, filename, accel, cached == -1 ? 1 : cached);
 	else if (endsWith(filename, ".svg"))
-		return loadSVG(result, filename, 1, width, height, 0);
+		return loadSVG(result, filename, cached == -1 ? 1 : cached, width, height, scale, keepAspect, align);
 	else if (endsWith(filename, ".jpg"))
-		return loadJPG(result, filename, 0);
+		return loadJPG(result, filename, cached == -1 ? 0 : cached);
 	else if (endsWith(filename, ".gif"))
-		return loadGIF(result, filename, accel, 0);
+		return loadGIF(result, filename, accel, cached == -1 ? 0 : cached);
 	return 0;
 }
 
