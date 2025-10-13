5e613eb1ac292a434d76142131454845b12b8054
fairbird <rrrr53@hotmail.com>
2025-10-13 23:56:13 +0300

    [Help] Improve action map processing This change optimizes the internal action map processing of the help functionality.
    cherry-pick > https://github.com/openatv/enigma2/commit/e628efc0145d157f00e90e57eccbee26ba5cfe34

--- lib/python/Screens/Screen.py
+++ lib/python/Screens/Screen.py
@@ -5,7 +5,7 @@ from enigma import eRCInput, eTimer, eWindow, getDesktop
 
 from skin import GUI_SKIN_ID, applyAllAttributes
 from skin import GUI_SKIN_ID, applyAllAttributes, menus, screens, setups
-from Components.ActionMap import ActionMap
+from Components.ActionMap import HelpableActionMap
 from Components.config import config
 from Components.GUIComponent import GUIComponent
 from Components.Pixmap import Pixmap
@@ -65,9 +65,10 @@ class Screen(dict):
 		self.ignoreWidgets = []
 		self.setImage(className)
 		if enableHelp:
-			self["helpActions"] = ActionMap(["HelpActions"], {
+			self["helpAction"] = HelpableActionMap(self, ["HelpAction"], {
+				# "displayHelp": (self.showHelp, _("Display the context sensitive help screen"))
 				"displayHelp": self.showHelp
-			}, prio=0)
+			}, prio=0, description=_("Help Action"))
 			self["key_help"] = StaticText(_("HELP"))
 
 	def __repr__(self):
