f60664f6348d53ca740eee6545db2149707166c6
jbleyel <jbleyel@gmx.net>
2025-10-21 13:46:05 +0200

    "[RatingIconLabel]"
    * fix color
    * improve posioning

--- lib/python/Components/Renderer/RatingIconLabel.py
+++ lib/python/Components/Renderer/RatingIconLabel.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from re import search
 from enigma import eLabel, gRGB
 
 from skin import parseColor
@@ -13,9 +14,10 @@ class RatingIconLabel(Renderer):
 		self.colors = {}
 		self.extendDirection = "right"
 		self.sidesMargin = 20
+		self.initialWidth = 0
+		self.initialXPos = 0
 
 	def postWidgetCreate(self, instance):
-		instance.setNoWrap(True)
 		self.changed((self.CHANGED_DEFAULT,))
 
 	def applySkin(self, desktop, parent):
@@ -32,6 +34,8 @@ class RatingIconLabel(Renderer):
 					attribs.append((attrib, value))
 		self.skinAttributes = attribs
 		result = Renderer.applySkin(self, desktop, parent)
+		self.initialWidth = self.instance.size().width()
+		self.initialXPos = self.instance.position().x()
 		self.changed((self.CHANGED_DEFAULT,))
 		return result
 
@@ -58,20 +62,22 @@ class RatingIconLabel(Renderer):
 							self.hideLabel()
 							return
 					else:
-						age = int(self.source.text.replace("+", ""))
+						age = int(search(r"\d+", self.source.text.replace("+", "")).group())
 						if age <= 15:
 							age += 3
 						ageText = str(age)
 						color = self.colors.get(age, 0x10000000)
 					size = self.instance.size()
 					pos = self.instance.position()
+					self.instance.setNoWrap(True)
 					self.instance.setText(ageText)
 					textSize = self.instance.calculateSize()
+					self.instance.setNoWrap(False)
 					newWidth = textSize.width() + self.sidesMargin
-					if newWidth < size.width():
-						newWidth = size.width()
+					if newWidth < self.initialWidth:
+						newWidth = self.initialWidth
 					if self.extendDirection == "left":
-						rightEdgePos = pos.x() + size.width()
+						rightEdgePos = self.initialXPos + self.initialWidth
 						self.move(rightEdgePos - newWidth, pos.y())
 					if self.extendDirection != "none":
 						self.resize(newWidth, size.height())
