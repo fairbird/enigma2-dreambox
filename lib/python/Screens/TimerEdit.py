f07efee456e91f5050394726310888d44505d0b8
fairbird <rrrr53@hotmail.com>
2025-11-06 23:09:05 +0300

    remove menu key definition; screens don't have a menu

--- lib/python/Screens/TimerEdit.py
+++ lib/python/Screens/TimerEdit.py
@@ -48,7 +48,6 @@ class TimerEditList(Screen):
 		self["key_green"] = StaticText(_("Add"))
 		self["key_yellow"] = StaticText("")
 		self["key_blue"] = StaticText("")
-		self["key_menu"] = StaticText(_("MENU"))
 		self["key_info"] = StaticText(_("INFO"))
 
 		self["description"] = Label("")
