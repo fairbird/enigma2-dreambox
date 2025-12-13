26f53560e1cb3e98d7f5bde845cae2205a5a51e9
jbleyel <jbleyel@gmx.net>
2025-12-13 22:18:20 +0100

    "[eDVBServiceStream]" add missing frontendInfo
    Thanks @WXbet

--- lib/service/servicedvbstream.cpp
+++ lib/service/servicedvbstream.cpp
@@ -412,3 +412,9 @@ void eDVBServiceStream::gotNewEvent(int /*error*/)
 
 	/* TODO: inject EIT section into the stream */
 }
+
+RESULT eDVBServiceStream::frontendInfo(ePtr<iFrontendInformation> &ptr)
+{
+	ptr = this;
+	return 0;
+}
