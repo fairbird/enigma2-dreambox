5c325ce4427a268916e06fd069b4bb2a19d78a3d
Hains van den Bosch <hainsvdbosch@ziggo.nl>
2026-01-08 15:21:57 +0100

    EpgList.py: Use in keyword
    Cosmetic. No change to functionallity.
    As suggested by @Huevos.

--- lib/python/Components/EpgList.py
+++ lib/python/Components/EpgList.py
@@ -63,7 +63,7 @@ class EPGList(GUIComponent):
 		elif type == EPG_TYPE_MULTI:
 			self.l.setBuildFunc(self.buildMultiEntry)
 		else:
-			assert (type == EPG_TYPE_SIMILAR or type == EPG_TYPE_PARTIAL)
+			assert type in (EPG_TYPE_SIMILAR, EPG_TYPE_PARTIAL)
 			self.l.setBuildFunc(self.buildSimilarEntry)
 		self.epgcache = eEPGCache.getInstance()
 		main_icons = (
