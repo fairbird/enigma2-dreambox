5706d752ebe115b1c6ab39e0be2990df89339a08
fairbird <rrrr53@hotmail.com>
2026-01-19 03:43:40 +0300

    [ServiceHelper] Fix crash 2026-01-19 02:11:07+0300 [-]   File "/usr/lib/enigma2/python/Tools/ServiceHelper.py", line 17, in _action 2026-01-19 02:11:07+0300 [-]     self.deamonSocket.connect("/tmp/deamon.socket") 2026-01-19 02:11:07+0300 [-]     ~~~~~~~~~~~~~~~~~~~~~~~~~^^^^^^^^^^^^^^^^^^^^^^ 2026-01-19 02:11:07+0300 [-] FileNotFoundError: [Errno 2] No such file or directory

--- lib/python/Tools/ServiceHelper.py
+++ lib/python/Tools/ServiceHelper.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import os
+
 from socket import socket, AF_UNIX, SOCK_STREAM
 from twisted.internet.reactor import callInThread
 
@@ -14,9 +16,15 @@ class ServiceHelper:
 
 	def _action(self, action):
 		self.deamonSocket = socket(AF_UNIX, SOCK_STREAM)
-		self.deamonSocket.connect("/tmp/deamon.socket")
-		self.deamonSocket.send(f"{action},{self.serviceName}".encode())
-		self._waitSocket()
+		if not os.path.exists("/tmp/deamon.socket"):
+			if self.callback: self.callback()
+			return
+		try:
+			self.deamonSocket.connect("/tmp/deamon.socket")
+			self.deamonSocket.send(f"{action},{self.serviceName}".encode())
+			self._waitSocket()
+		except:
+			if self.callback: self.callback()
 
 	def restart(self, callback, timeout=5000):
 		self.callback = callback
