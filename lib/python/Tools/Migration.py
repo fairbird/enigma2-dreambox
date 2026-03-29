501abf3ac6e43c0960c99a5016cf3fd408577fa0
fairbird <rrrr53@hotmail.com>
2026-03-29 22:25:03 +0300

    [Migration] finally fix Filecommander settings migration cherry-picked > https://github.com/openatv/enigma2/commit/dfc5c0a339759baaae32fddf73aa65f3e4ed3396

--- lib/python/Tools/Migration.py
+++ lib/python/Tools/Migration.py
@@ -133,6 +133,7 @@ def migrateFileCommander():
 		("sortFiles_right", "sortFilesRight"),
 		("unknown_extension_as_text", "useViewerForUnknown")
 	)
+	config.plugins.FileCommander = ConfigSubsection()
 	config.plugins.filecommander = ConfigSubsection()
 	for old, new in attributes:
 		setattr(config.plugins.filecommander, old, ConfigText(default=""))
@@ -147,3 +148,4 @@ def migrateFileCommander():
 			getattr(config.plugins.FileCommander, new).value = value
 		getattr(config.plugins.filecommander, old).value = ""
 	config.plugins.filecommander.save()
+	config.plugins.FileCommander.save()
