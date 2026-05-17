a86104b22bd91b93e8e9f7f8004e18860cc7a50e
jbleyel <jbleyel@gmx.net>
2026-05-17 07:33:52 +0200

    "[skin]" add conditional attribute for TemplateParser

--- lib/python/skin.py
+++ lib/python/skin.py
@@ -1928,7 +1928,7 @@ class SkinContextHorizontal(SkinContext):
 		return (SizeTuple(pos), SizeTuple(size))
 
 
-class TemplateParser():
+class TemplateParser:
 	def __init__(self, debug=False):
 		self.debug = debug
 		self.processors = {
@@ -2028,9 +2028,12 @@ class TemplateParser():
 		size = None
 		skinAttributes = []
 		itemIndex = ""
+		conditional = None
 		for attrib, value in node.items():  # Walk all attributes.
 			if attrib not in ignore:
 				match attrib:
+					case "conditional":
+						conditional = value
 					case "position":
 						pos = value
 					case "size":
@@ -2040,6 +2043,15 @@ class TemplateParser():
 						skinAttributes.append((attrib, value))
 					case _:
 						skinAttributes.append((attrib, value))
+
+		if conditional is not None:
+			try:
+				if not eval(conditional):
+					return []
+			except Exception as err:
+				skinError(f"collectAttributes 'conditional' '{conditional}' resulted in error '{err}'")
+				return []
+
 		if itemIndex and includeItemIndexes and itemIndex not in includeItemIndexes:
 			return []
 		if itemIndex and excludeItemIndexes and itemIndex in excludeItemIndexes:
