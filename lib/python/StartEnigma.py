d0ed4f043772e46c4dcac8fee46842840ce58839
fairbird <rrrr53@hotmail.com>
2026-05-23 11:13:53 +0300

    [Debug] * add new config.crash.debugTextEncoding setting * add missing restart attribute for some debug settings
    cherry-picked > https://github.com/openatv/enigma2/commit/892ccbb9778ece1e7a585cdfec748ea391296d53

--- lib/python/StartEnigma.py
+++ lib/python/StartEnigma.py
@@ -36,6 +36,7 @@ config.crash.debugTimers = ConfigYesNo(default=False)
 config.crash.debugTeletext = ConfigYesNo(default=False)
 config.crash.debugStorage = ConfigYesNo(default=False)
 config.crash.debugDVBDB = ConfigYesNo(default=False)
+config.crash.debugTextEncoding = ConfigYesNo(default=False)
 
 # config.plugins needs to be defined before InputDevice < HelpMenu < MessageBox < InfoBar.
 config.plugins = ConfigSubsection()
