bb0598031442b7a5a07bc3afd92e35b54794544a
jbleyel <jbleyel@gmx.net>
2026-07-12 12:39:22 +0200

    "[font]" Add support for resetting text color via escape sequence
    Introduce GS_COLORRESET flag so an incomplete/invalid color escape
    (\x8a not followed by 8 valid hex digits) resets the foreground color
    back to default instead of being silently dropped, mirroring how
    GS_COLORCHANGE applies an explicit color.
    
    Cherry pick -> https://github.com/OpenViX/enigma2/commit/25230f7066c0d95341ad8f2c453621b1512542dc

--- lib/gdi/font.cpp
+++ lib/gdi/font.cpp
@@ -830,6 +830,7 @@ int eTextPara::renderString(const char *string, int rflags, int border, int mark
 
 	unsigned long newcolor = 0;
 	bool activate_newcolor = false;
+	bool activate_colorreset = false;
 	int nextflags = 0;
 	int pos = 0;
 	int markedlen = 0;
@@ -874,13 +875,20 @@ int eTextPara::renderString(const char *string, int rflags, int border, int mark
 							{
 								if ((i + 2 + codeidx) == uc_visual.end()) break;
 								color[codeidx] = (char)((*(i + 2 + codeidx)) & 0xff);
+								if (!isxdigit((unsigned char)color[codeidx]))
+									break;
 							}
+							isprintable = 0;
 							if (codeidx == 8)
 							{
 								newcolor = gRGB(color).argb();
 								activate_newcolor = true;
-								isprintable = 0;
 								i += 1 + codeidx;
+							}
+							else
+							{
+								activate_colorreset = true;
+								i++;
 							}
 							break;
 						}
@@ -929,6 +937,9 @@ nprint:				isprintable=0;
 		}
 		if (isprintable)
 		{
+			if (activate_colorreset)
+				flags |= GS_COLORRESET;
+
 			if (markedpos == -2 || markedpos == pos++)
 			{
 				flags |= GS_INVERT;
@@ -971,6 +982,7 @@ nprint:				isprintable=0;
 			{
 				nextflags = 0;
 				activate_newcolor = false;
+				activate_colorreset = false;
 			}
 		} else if (nextflags&GS_ISFIRST && !glyphs.empty())
 		{
@@ -1049,10 +1061,15 @@ void eTextPara::blit(gDC &dc, const ePoint &offset, const gRGB &cbackground, con
 			line_offs = *(line_offs_it++);
 			line_chars = *(line_chars_it++);
 		}
-		if (i->flags & GS_COLORCHANGE)
+		/* don't do colorchanges in borders */
+		if (!border)
 		{
-			/* don't do colorchanges in borders */
-			if (!border)
+			if (i->flags & GS_COLORRESET)
+			{
+				currentforeground = foreground;
+				setcolor = true;
+			}
+			else if (i->flags & GS_COLORCHANGE)
 			{
 				currentforeground = i->newcolor;
 				setcolor = true;
