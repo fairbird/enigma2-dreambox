64bf5e6c79ec6f3de421fea5281caa44125603fe
fairbird <rrrr53@hotmail.com>
2026-07-13 22:30:21 +0300

    [Plugin] Small fix

--- lib/python/Plugins/Plugin.py
+++ lib/python/Plugins/Plugin.py
@@ -88,7 +88,7 @@ class PluginDescriptor:
 
 	DO_CLOSE_RECURSIVE = 2
 
-	def __init__(self, name="Plugin", where=[], description="", icon=None, fnc=None, wakeupfnc=None, needsRestart=None, internal=False, weight=0, closeMode=0)):
+	def __init__(self, name="Plugin", where=[], description="", icon=None, fnc=None, wakeupfnc=None, needsRestart=None, internal=False, weight=0, closeMode=0):
 		self.name = name
 		self.internal = internal
 		self.needsRestart = needsRestart
