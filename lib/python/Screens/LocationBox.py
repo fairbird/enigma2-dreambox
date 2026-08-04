f704ff8b5c90abe621f0e3d42bc9c20f1162c315
ims <ims21@users.sourceforge.net>
2026-08-04 21:22:42 +0200

    LocationBox - fix blue button

--- lib/python/Screens/LocationBox.py
+++ lib/python/Screens/LocationBox.py
@@ -213,7 +213,7 @@ class LocationBox(Screen, NumericalTextInput):
 			self.currList = "filelist"
 			self["filelist"].selectionEnabled(1)
 			self["booklist"].selectionEnabled(0)
-			self["key_blue"].setText(_("Add bookmark" if self.realBookmarks else None))
+			self["key_blue"].setText(_("Add bookmark") if self.realBookmarks else '')
 			self.updateTarget()
 
 	def switchToBookList(self):
