75c25d1a97c68b74514f6c8e2e992fed8edef820
fairbird <rrrr53@hotmail.com>
2026-08-07 19:36:53 +0000

    PEP8 double aggressive E251 and E252

--- lib/python/Components/Converter/MenuGrid_MenuIcon.py
+++ lib/python/Components/Converter/MenuGrid_MenuIcon.py
@@ -60,15 +60,15 @@ def getMenuIcon(menuEntryID):
 				skinPath = v
 				break
 	if skinPath:
-		png = LoadPixmap(cached = True, path = resolveFilename(SCOPE_GUISKIN, skinPath))
+		png = LoadPixmap(cached=True, path=resolveFilename(SCOPE_GUISKIN, skinPath))
 	if png == None:
 		for name in (iconName, iconName.lower(), iconName.replace(" ", ""), iconName.replace(" ", "").lower()):
-			png = LoadPixmap(cached = True, path = resolveFilename(SCOPE_GUISKIN, iconPath + name + ".png"))
+			png = LoadPixmap(cached=True, path=resolveFilename(SCOPE_GUISKIN, iconPath + name + ".png"))
 			if png:
 				break
 
 	if png == None:
-		png = LoadPixmap(cached = True, path = resolveFilename(SCOPE_GUISKIN, iconPath + "Undefined.png"))
+		png = LoadPixmap(cached=True, path=resolveFilename(SCOPE_GUISKIN, iconPath + "Undefined.png"))
 	return png
 
 # =========================================================
