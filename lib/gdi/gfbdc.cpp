af5434e1fbbad6255cc0f40002a62c4fd9be919f
fairbird <rrrr53@hotmail.com>
2026-09-17 13:33:56 +0300

    gfbdc

--- lib/gdi/gfbdc.cpp
+++ lib/gdi/gfbdc.cpp
@@ -47,6 +47,7 @@ gFBDC::gFBDC()
 	if (!((xres == 720 && yres == 576)
 		|| (xres == 1280 && yres == 720)
 		|| (xres == 1920 && yres == 1080)))
+		|| (xres == 3840 && yres == 2160)))
 	{
 		/* fallback to a decent default */
 		xres = 720;
